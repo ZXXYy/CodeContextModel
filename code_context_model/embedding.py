67b919b0d556b22210ccbfc881c66f973e6eff42
zxy <308169188@qq.com>
2024-06-27 13:59:14 +0800

    feat: embedding inference

--- code_context_model/embedding.py
+++ code_context_model/embedding.py
@@ -8,21 +8,21 @@ import numpy as np
 import random
 from tqdm import tqdm
 
-def get_nodes_text(expand_graph_path: str) -> str:
+def get_nodes_text(expand_graph_path: str) -> pd.DataFrame:
     tree = ET.parse(expand_graph_path)
     root = tree.getroot()
     nodes = root.findall(".//vertex")
-    nodes_text = []
+    nodes_id = []
     model_dir = expand_graph_path.split('/')[-3]
     codes_path = os.path.dirname(os.path.dirname(expand_graph_path)) + "/my_java_codes.tsv"
     # read tsv file
     df_code = pd.read_csv(codes_path, sep='\t')
     for vertex in nodes:
         node_id = '_'.join([model_dir, vertex.get('kind'), vertex.get('ref_id')]) 
-        node_text = df_code[df_code['id'] == node_id]['code'].values[0]
+        # node_text = df_code[df_code['id'] == node_id]['code'].values[0]
         # print(len(node_text))
-        nodes_text.append(node_text)
-    return nodes_text
+        nodes_id.append(node_id)
+    return df_code[df_code['id'].isin(nodes_id)]
 
 class TextEmbedding():
     def __init__(self):
@@ -36,15 +36,19 @@ class BgeEmbedding(TextEmbedding):
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
         self.model = AutoModel.from_pretrained('BAAI/bge-large-en-v1.5', revision="refs/pr/13")
+    
+    def __str__(self):
+        return "BgeEmbedding"
 
     def get_embedding(self, input_texts: str) -> torch.Tensor:  
         batch_dict = self.tokenizer(input_texts, padding=True, truncation=True, return_tensors='pt')
         with torch.no_grad():
             outputs = self.model(**batch_dict)
-            embeddings = outputs[0][:, 0] # FIXME: 如何根据单个embedding获取整个nodes的embedding，现在只取了第一个token的embedding
+            embeddings = outputs[0][:, 0] # FIXME: bge只需要取第一个token的embedding，不同的模型不一样
         embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
         print(embeddings.shape)
         print("Sentence embeddings:", embeddings)
+        return embeddings
 
 def get_tokenizer_data(input_texts):
     model = BgeEmbedding()
@@ -65,7 +69,7 @@ def get_tokenizer_statistics(input_dir):
         file = os.listdir(seed_dir)[random.randint(0, len(os.listdir(seed_dir)) - 1)]
         if file.endswith(".xml"):
             print(f"processing {seed_dir + file}")
-            nodes_text = get_nodes_text(seed_dir + file)
+            nodes_text = get_nodes_text(seed_dir + file)['code'].tolist()
             token_counts = get_tokenizer_data(nodes_text)
             total_token_counts.extend( [x for x in token_counts if x <= 2000])
         cnt += 1
@@ -79,9 +83,27 @@ def get_tokenizer_statistics(input_dir):
     plt.savefig("token_counts.png")
     plt.show()
 
+def embedding_inference(input_dir):
+    model = BgeEmbedding()
+    id_dirs = sorted(os.listdir(input_dir))
+    for id_dir in id_dirs:
+        print(input_dir, id_dir)
+        codes_path = os.path.join(input_dir, id_dir, "my_java_codes.tsv")
+        print(f"processing {codes_path}")
+        df_code = pd.read_csv(codes_path, sep='\t')
+        nodes_text = df_code['code'].tolist()
+        embeddings = model.get_embedding(nodes_text)
+        df_code['embedding'] = embeddings   
+        df_code = df_code.drop(columns=['code']) 
+        df_code.to_pickle(os.path.join(input_dir, id_dir, f'{model}_embedding.pkl'))
+        break
+            
+
+
 if __name__ == '__main__':
     # nodes_text = get_nodes_text("data/mylyn/60/seed_expanded/1_step_seeds_0_expanded_model.xml")
     # model = BgeEmbedding()
     # model.get_embedding(nodes_text)
     input_dir = "data/mylyn/"
+    embedding_inference(input_dir)
     
