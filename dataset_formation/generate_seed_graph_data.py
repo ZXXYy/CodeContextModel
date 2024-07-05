554053714d03a18422d3da506039e4f71907f420
zxy <308169188@qq.com>
2024-07-05 16:44:54 +0800

    collapsed variable nodes

--- dataset_formation/generate_seed_graph_data.py
+++ dataset_formation/generate_seed_graph_data.py
@@ -5,6 +5,7 @@ import os
 
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from tqdm import tqdm
 from graphviz import Digraph
@@ -53,6 +54,84 @@ def subgraph2graph(expanded_model_path):
     # write to file
     out_path = expanded_model_path.replace('new_', 'big_')
     tree.write(out_path, encoding='utf-8', xml_declaration=False)
+
+def collaspe_variables(expanded_model_path, code_path, model_dir, outdir):
+    df_code = pd.read_csv(code_path, sep='\t')
+    tree = ET.parse(expanded_model_path)
+    root = tree.getroot()
+    # 1. 找到所有的class结点
+    vertices_class = root.findall(".//vertex[@kind='class']")
+    logger.info(f"Number of class: {len(vertices_class)}")
+    for i, vertex in enumerate(vertices_class):
+        class_id = vertex.get('id')
+        # 2. 对于每一个class结点，找到其所有的variable结点
+        class_edges = root.findall(f".//edge[@start='{class_id}']")
+        variable_codes = ""
+        variable_ids = []
+        variable_origin = False
+        logger.info(f"Number of class edges: {len(class_edges)}")
+        for edge in class_edges:
+            end_id = edge.get('end')
+            variable = root.find(f".//vertex[@id='{end_id}'][@kind='variable']")
+            if variable is None:
+                continue
+            logger.info(f"class declared variables: {variable}")
+            variable_id = f"{model_dir}_{variable.get('kind')}_{variable.get('ref_id')}"
+            variable_code = df_code[df_code['id'] == variable_id]['code'].values[0]
+            # 2.1 如果len(variable_code) < 100, 则将variable结点合并
+            if len(variable_code) < 100:
+                # 2.1.1 找到这些结点对应的代码片段，合并
+                variable_codes += variable_code + "\n"
+                variable_ids.append(variable.get('id'))
+                variable_origin = variable_origin or variable.get('origin') == '1'
+                pass
+            # 2.2 如果len(variable_code) >= 100, variable结点保存
+            else:
+                pass
+        new_variable_id = len(root.findall(".//vertex"))
+        new_variable = ET.Element("vertex", attrib={
+            "id": str(new_variable_id),
+            "kind": "variable",
+            "origin": "1" if variable_origin else "0",
+            "ref_id": f"collapsed_variable_{i}",
+        })
+        vertices = root.find('.//vertices')
+        vertices.append(new_variable)
+        df_code = pd.concat([df_code, pd.DataFrame({
+            'id': f"{model_dir}_{new_variable.get('kind')}_{new_variable.get('ref_id')}", 
+            'code': variable_codes}, index=[0])],ignore_index=True)
+        # 3. 获取删除结点的所有边，将这些边的start和end指向新的variable结点
+        for variable_id in variable_ids:
+            variable = root.find(f".//vertex[@id='{variable_id}']")
+            parent_variable = root.find(f".//vertices")
+            parent_variable.remove(variable)
+            # 删除边
+            def update_edges(edges, variable_id):
+                parent_edges = root.find(f".//edges")
+                for edge in edges:
+                    start_id = edge.get('start')
+                    end_id = edge.get('end')
+                    if start_id == variable_id:
+                        # 如果边已经存在，则不添加
+                        if root.find(f".//edge[@start='{new_variable_id}'][@end='{end_id}']") is None:
+                            edge.set('start', str(new_variable_id))
+                        else:
+                            parent_edges.remove(edge)
+                    if end_id == variable_id:
+                        if root.find(f".//edge[@start='{start_id}'][@end='{new_variable_id}']") is None:
+                            edge.set('end', str(new_variable_id))
+                        else:
+                            parent_edges.remove(edge)
+            start_edges = root.findall(f".//edge[@start='{variable_id}']")
+            end_edges = root.findall(f".//edge[@end='{variable_id}']")
+            update_edges(start_edges, variable_id)
+            update_edges(end_edges, variable_id)
+
+    # 5. 写入文件
+    tree.write(f"{outdir}/collaspe_vertex.xml", encoding='utf-8', xml_declaration=True)
+    df_code.to_csv(f"{outdir}/my_java_codes_collapse.tsv", sep='\t', index=False)
+            
+
 
 def generate_seed(expanded_model_path, step=1):
     tree = ET.parse(expanded_model_path)
@@ -187,9 +266,16 @@ def get_statistics(input_path):
         'classes': [],
         'variable_percentage': [],
     }
+    variables_code = []
     for context_model in tqdm(context_models):
         logger.debug(f"Processing {context_model}")
-        # get the number of variables
+        if not os.path.exists(f"{input_path}/{context_model}/my_java_codes.tsv"):
+            continue
+        df_code = pd.read_csv(f"{input_path}/{context_model}/my_java_codes.tsv", sep='\t')
+        for i, row in df_code.iterrows():
+            if 'variable' in row['id'] :
+                variables_code.append(len(row['code']))
+
         if not os.path.exists(f"{input_path}/{context_model}/big_1_step_expanded_model.xml"):
             continue
         tree = ET.parse(f"{input_path}/{context_model}/big_1_step_expanded_model.xml")
@@ -228,16 +314,29 @@ def get_statistics(input_path):
     # count the number of variable_percentage > 0.5
     num_variable_percentage = len([i for i in statistics['variable_percentage'] if i > 0.2])
     print(f"variable_percentage > 0.2: {num_variable_percentage}")
+    variables_code_lens = [x for x in variables_code if x <= 1024]
+    print(f"variables code length <= 1024: {len(variables_code_lens)}/{len(variables_code)}")
+    variables_code_lens = [x for x in variables_code if x <= 200]
+    print(f"variables code length <= 200: {len(variables_code_lens)}/{len(variables_code)}")
+    variables_code_lens = [x for x in variables_code if x <= 100]
+    print(f"variables code length <= 100: {len(variables_code_lens)}/{len(variables_code)}")
     # plot the statistics num
     count_values = [statistics['positive_variables'].count(i) for i in range(0, max(statistics['positive_variables'])+1)]
 
     # plt.bar(range(0, max(statistics['positive_variables'])+1), count_values)
-    plt.hist(statistics['variable_percentage'], bins=50, alpha=0.5, label='variables')
+    # plt.hist(statistics['variable_percentage'], bins=50, alpha=0.5, label='variables')
+    # plt.hist(variables_code, bins=50, alpha=0.5, label='variables')
+
     plt.show()
 
 if __name__ == '__main__':
-    main()
-
+    # main()
+    # get_statistics()
+    # expanded_model_path = "/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/60/big_1_step_expanded_model.xml"
+    # code_path = "/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/60/my_java_codes.tsv"
+    # model_dir = "60" 
+    # outdir = "/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/60"
+    # collaspe_variables(expanded_model_path, code_path=code_path, model_dir=model_dir, outdir=outdir)
     # 使用下述代码转化子图为一张完整的图
     # input_path = '/data0/xiaoyez/CodeContextModel/data/repo_first_3'
     # context_models = os.listdir(input_path)
@@ -248,4 +347,4 @@ if __name__ == '__main__':
 
     # 使用下述代码展示图     
     # display_graph('/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/1729/seed_expanded/1_step_seeds_0_expanded_model.xml')
-    # display_graph('/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/1729/big_1_step_expanded_model.xml')
+    display_graph('/Users/zhengxiaoye/Desktop/codeContextGNN/CodeContextModel/data/mylyn/60/collaspe_vertex.xml')
