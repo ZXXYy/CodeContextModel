12e844c006c94d372ea1a7c5ce4d9e99758b094e
xiaoyez <xiaoyez@zju.edu.cn>
2024-07-07 23:35:16 +0800

    parallel embedding

--- code_context_model/embedding.py
+++ code_context_model/embedding.py
@@ -1,9 +1,10 @@
 
 import os
+import argparse
 # os.environ['CUDA_VISIBLE_DEVICES'] = '8'
 import random
-import click
 import logging
+import multiprocessing
 
 import torch
 import pandas as pd
@@ -17,6 +18,7 @@ from transformers import AutoModel, AutoTokenizer
 logging.basicConfig(level=logging.INFO, format='[%(filename)s:%(lineno)d] - %(message)s')
 logger = logging.getLogger('Eembedding')
 
+
 class TextEmbedding():
     def __init__(self):
         self.tokenizer = None
@@ -26,19 +28,17 @@ class TextEmbedding():
 
 
 class BgeEmbedding(TextEmbedding):
-    def __init__(self):
+    def __init__(self, device):
         self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
         self.model = AutoModel.from_pretrained('BAAI/bge-large-en-v1.5', revision="refs/pr/13")
-        self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device(f"cuda:{device}" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
         logger.info(f"BgeEmbedding initialized in {self.device}")
     
     def __str__(self):
         return "BgeEmbedding"
 
-    def get_embedding(self, input_texts: list, batch_size: int = 32) -> torch.Tensor:
-        logger.info(f"Total number of input texts: {len(input_texts)}")
-        
+    def get_embedding(self, input_texts: list, batch_size: int = 32) -> torch.Tensor:        
         # Function to process a single batch and get embeddings
         def process_batch(batch_texts):
             batch_dict = self.tokenizer(batch_texts, padding=True, truncation=True, return_tensors='pt')
@@ -117,15 +117,14 @@ def get_tokenizer_statistics(input_dir):
     plt.savefig("token_counts.png")
     plt.show()
 
-def embedding_inference(input_dir, output_dir):
-    model = BgeEmbedding()
+def read_data(input_dir, output_dir, model_name):
     id_dirs = sorted(os.listdir(input_dir))
-    codes = []
+    dataset = []
     logger.info("Start reading codes...")
     for id_dir in tqdm(id_dirs):
         logger.debug(f"{input_dir}, {id_dir}")
         codes_path = os.path.join(input_dir, id_dir, "my_java_codes_collapse.tsv")
-        output_fn = os.path.join(output_dir, f'{id_dir}_{model}_embedding.pkl')
+        output_fn = os.path.join(output_dir, f'{id_dir}_{model_name}_embedding.pkl')
         if not os.path.exists(codes_path):
             logger.info(f"No file in {codes_path}")
             continue
@@ -133,41 +132,87 @@ def embedding_inference(input_dir, output_dir):
             logger.info(f"Skip {output_fn}")
             continue
         df_code = pd.read_csv(codes_path, sep='\t')
-        codes.append({
+        dataset.append({
             'id_dir': id_dir,
-            'df_code': df_code
+            'df_code': df_code,
+            'output_fn': output_fn
         })
         logger.debug(f"df_code shape: {df_code.shape}")
-        
-    logger.info(f"start inference...")
-    for i, code in tqdm(enumerate(codes), total=len(codes)):
+    
+    return dataset
+
+
+def inference_dataset(process_idx, model, dataset):
+    for i, code in tqdm(enumerate(dataset), total=len(dataset), desc=f"Process {process_idx}", position=process_idx):
+        id_dir = code['id_dir']
         df_code = code['df_code']
         nodes_text = df_code['code'].tolist()
+        # logger.info(f"Process {process_idx} processing {id_dir}, number of input texts: {len(input_texts)}")
         embeddings = model.get_embedding(nodes_text)
         df_code.loc[:, 'embedding'] = embeddings   
         df_code = df_code.drop(columns=['code']) 
         code['df_code'] = df_code
-        codes[i] = code
+        dataset[i] = code
 
-    logger.info(f"write embeddings to file")
-    for i, code in tqdm(enumerate(codes), total=len(codes)):
-        id_dir = code['id_dir']
-        output_fn = os.path.join(output_dir, f'{id_dir}_{model}_embedding.pkl')
+    return dataset
+
+def write_to_file(dataset):
+    for i, code in tqdm(enumerate(dataset), total=len(dataset)):
+        output_fn = code['output_fn']
         df_code = code['df_code']
         df_code.to_pickle(output_fn)
-        df_code.to_csv(output_fn.replace('.pkl', '.csv'), index=False)
+        # df_code.to_csv(output_fn.replace('.pkl', '.csv'), index=False)
 
-@click.command()
-@click.option('--input_dir', type=str, help='input directory')
-@click.option('--output_dir', type=str, help="ouptut dir")
-def main(input_dir, output_dir):
-    embedding_inference(input_dir, output_dir)
+
+def embedding_inference(input_dir, output_dir, debug=False):
+    # init models
+    models = []
+    for i in range(10):
+        models.append(BgeEmbedding(device=i))
+    
+    # read data
+    codes = read_data(input_dir, output_dir, f"{models[0]}")
+    if debug:
+        codes = codes[:30]
+    logger.info(f"Total number of datasets: {len(codes)}")
+
+    # split to length of models splits
+    codes_splits = [codes[i:i + len(codes)//len(models)] for i in range(0, len(codes), len(codes)//len(models))]
+    if len(codes_splits) > len(models):
+        codes_splits[-2] += codes_splits[-1]
+        codes_splits.pop()
+
+    assert sum([len(split) for split in codes_splits]) == len(codes)
+    assert len(models) == len(codes_splits)
+
+    logger.info(f"start inference...")
+
+    
+    tasks = list(zip(range(len(models)), models, codes_splits))
+    with multiprocessing.Pool(processes=len(models)) as pool:
+        results =  pool.starmap(inference_dataset, tasks) 
+    print(results)
+    # List of List to list
+    results = [item for sublist in results for item in sublist]
+    
+    assert len(results) == len(codes), f"{len(results)} != {len(codes)}"
+    write_to_file(results)
+    logger.info(f"write embeddings finished")
+
 
 
 if __name__ == '__main__':
     # nodes_text = get_nodes_text("data/mylyn/60/seed_expanded/1_step_seeds_0_expanded_model.xml")
     # model = BgeEmbedding()
     # model.get_embedding(nodes_text)
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='data', help='input directory')
+    parser.add_argument('--output_dir', type=str, default='data', help='output directory')
+    parser.add_argument('--debug', action='store_true', help='debug mode')
+    args = parser.parse_args()
+
+    multiprocessing.set_start_method('spawn')
+    embedding_inference(args.input_dir, args.output_dir, args.debug)
+
 
     
